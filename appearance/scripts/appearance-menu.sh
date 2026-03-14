cb5355dc26f0f0c4e354cc50b4586d7684e848e3
Bak0 <57047165+Bak0@users.noreply.github.com>
2026-03-14 18:44:29 +0000

    Update appearance-menu.sh

--- appearance/scripts/appearance-menu.sh
+++ appearance/scripts/appearance-menu.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-main_choice="$(printf 'Theme switch\nWaybar' | rofi -dmenu -i -p 'Appearance')"
+main_choice="$(printf 'Theme switch\nWaybar\nRofi\nBackgrounds' | rofi -dmenu -i -p 'Appearance')"
 [[ -n "${main_choice:-}" ]] || exit 0
 
 case "${main_choice}" in
@@ -18,6 +18,32 @@ case "${main_choice}" in
                 ;;
             "Style")
                 "${HOME}/wkstation/appearance/scripts/waybar-style-menu.sh"
+                ;;
+        esac
+        ;;
+    "Rofi")
+        sub_choice="$(printf 'Style\nLayout' | rofi -dmenu -i -p 'Rofi')"
+        [[ -n "${sub_choice:-}" ]] || exit 0
+
+        case "${sub_choice}" in
+            "Style")
+                "${HOME}/wkstation/appearance/scripts/rofi-style-menu.sh"
+                ;;
+            "Layout")
+                "${HOME}/wkstation/appearance/scripts/rofi-layout-menu.sh"
+                ;;
+        esac
+        ;;
+    "Backgrounds")
+        sub_choice="$(printf 'Still\nAnimated' | rofi -dmenu -i -p 'Backgrounds')"
+        [[ -n "${sub_choice:-}" ]] || exit 0
+
+        case "${sub_choice}" in
+            "Still")
+                "${HOME}/wkstation/appearance/scripts/background-still-menu.sh"
+                ;;
+            "Animated")
+                "${HOME}/wkstation/appearance/scripts/background-animated-menu.sh"
                 ;;
         esac
         ;;
